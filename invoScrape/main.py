5fb1db5a965f144f31261a39a0c0428dbce4c6c0
Itay Peleg <135420032+ItayPeleg@users.noreply.github.com>
2023-07-22 12:11:23 +0300

    Add files via upload

---
+++ invoScrape/main.py
@@ -0,0 +1,18 @@
+import logging
+import threading
+import time
+from file_watcher import start_watching
+
+
+def main():
+    path = r"C:\Web\uploads"
+    t = threading.Thread(target=start_watching, args=(path,))
+    t.start()
+
+    while True:
+        logging.info('Main thread is still running...')
+        time.sleep(120)
+
+
+if __name__ == '__main__':
+    main()
